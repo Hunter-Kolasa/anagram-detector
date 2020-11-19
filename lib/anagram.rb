4412d6b6759746b377502b888dc41c7d8dbd8934
Hunter <kolasa.hunter@aol.com>
2020-11-19 13:47:14 -0500

    commit tree 5da8b8920225406845196a217c1393952fe4cd90

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(word_array)
+        word_array.each.find_all do |array_word|
+            if array_word.split("").sort == word.split("").sort
+                yeet = array_word.split.flatten
+                yeet
+            end
+        end
+    end
+end
